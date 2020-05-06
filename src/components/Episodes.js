cb2b203dec20f2bcd88e596ffa06e48e992199d2
Russell Smith <russellsmith@MacBook-Pro.local>
2020-05-06 18:02:48 -0400

    created test for App and Episodes component

--- src/components/Episodes.js
+++ src/components/Episodes.js
@@ -1,11 +1,11 @@
-import React from 'react';
-import parse from 'html-react-parser';
+import React from "react";
+import parse from "html-react-parser";
 
 export default function Episodes(props) {
   return (
-    <div className="episodes">
+    <div data-testid="episodes" className="episodes">
       {props.episodes.map(e => (
-        <div className="episode" key={e.id}>
+        <div data-testid="episode" className="episode" key={e.id}>
           {e.image && (
             <img className="episode-image" src={e.image.medium} alt={e.name} />
           )}
